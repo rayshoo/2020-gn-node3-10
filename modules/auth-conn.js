2e7d1f0dc946677a6e496f9199aa9a657246c2a0
dfnk5516 <rayshoo@naver.com>
2020-06-21 10:53:24 +0900

    origin login, kakao 로그인페이지구현

--- modules/auth-conn.js
+++ modules/auth-conn.js
@@ -1,7 +1,8 @@
 const { alert } = require('./utils');
 
 const isUser = (req, res, next) => {
-  if(req.session && req.session.user) next();
+  // if(req.session && req.session.user) next();
+  if(req.isAuthenticated()) next();
   else {
      res.send(alert('회원만 사용하실 수 있습니다. 로그인 해주세요', '/'));
   }
@@ -11,14 +12,16 @@ const isUser = (req, res, next) => {
 }
 
 const isGuest = (req, res, next) =>{
-  if(!req.session.user) next();
+  // if(!req.session.user) next();
+  if(!req.isAuthenticated()) next();
   else{
     res.send(alert('정상적인 접근이 아닙니다. 로그아웃 이후에 사용하세요', '/board'));
   }
 }
 
 const isGrant2 = (req, res, next) =>{
-  if(req.session && req.session.user && req.session.user.grant >= 2) next();
+  // if(req.session && req.session.user && req.session.user.grant >= 2) next();
+  if(req.isAuthenticated() && req.session.user.grant >= 2) next();
   else{
     res.send(alert('정상적인 접근이 아닙니다. 로그아웃 이후에 사용하세요', '/board'));
   }
